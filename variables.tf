d47254a15d6ce3d6e38ed7ca5f02a304cabfbd5f
bhavis-triggers <bhavithraselvaraj73@gmail.com>
2025-11-01 14:28:20 +0530

    auth setup

--- variables.tf
+++ variables.tf
@@ -2,20 +2,21 @@ variable "gcp_project_id" {
   description = "The GCP project ID"
   type        = string
   validation{
-    condition = length(var.gcp_project_id) > 6 && length(var.gcp_project_id) <=20
+    condition = length(var.gcp_project_id) > 6 && length(var.gcp_project_id) <=25
     error_message = "Project ID must be between 6 to 20"
   }
-  default = "gcp_test_bigq_dep"
+  default = "gleaming-nomad-474505-r3"
 }
 variable "gcp_region" {
   description = "The GCP region"
   type        = string
   default = "asia-south2"
 }   
-/*variable "gcp_svc_key" {
+variable "gcp_svc_key" {
   description = "Path to the GCP service account key file"
   type        = string
-}*/
+  default = "bhavis-triggers/BigQuery/gleaming-nomad-474505-r3-fb906b46b1b7.json"
+}
 
 variable "env"{
     type = string

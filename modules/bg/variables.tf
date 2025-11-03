70237c08a1aefa8c877e58100d4088929128b558
bhavis-triggers <bhavithraselvaraj73@gmail.com>
2025-11-03 15:05:43 +0530

    correction#3

--- modules/bg/variables.tf
+++ modules/bg/variables.tf
@@ -6,10 +6,8 @@ variable "dataset_id"{
 variable "labels"{
     type=map(string)
     validation {
-    condition = contains(keys(var.labels), "app-id")
-            && contains(keys(var.labels), "app-own")
-            && contains(keys(var.labels), "res-name")
-    error_message = "You must provide mandatory labels: environment and managed_by."
+        condition = contains(keys(var.labels), "app-id") && contains(keys(var.labels), "app-own") && contains(keys(var.labels), "res-name")
+        error_message = "You must provide mandatory labels: environment and managed_by."
   }
 }
 variable "gcp_project_id" {

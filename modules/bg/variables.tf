e9abc6c3039aa0135a7da3f3cec01ad777b1cda0
bhavis-triggers <bhavithraselvaraj73@gmail.com>
2025-11-03 16:00:15 +0530

    removed validation

--- modules/bg/variables.tf
+++ modules/bg/variables.tf
@@ -40,14 +40,14 @@ variable "dataset"{
         dataset_name = optional(string)
         dataset_desc = optional(string)
         labels = map(string)
-        validation {
-        condition = contains(keys("app-own"), var.dataset.labels) && contains(keys("res-name"), var.dataset.labels)
-        error_message = "You must provide mandatory labels: environment and managed_by."
-        }
         /*iam_bindings = optional(list(object{
             role = optional(string)
             group_by_email = optional(string)
             user_by_email = optional(string)
-        }))*/
+        }))
+        validation {
+        condition = contains(keys("app-own"), var.dataset.labels) && contains(keys("res-name"), var.dataset.labels)
+        error_message = "You must provide mandatory labels: environment and managed_by."
+        }*/
     }))
 }

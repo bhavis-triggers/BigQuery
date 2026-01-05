b050174b4753791a14d9b9a1b989dd8a78d74f93
bhavis-triggers <bhavithraselvaraj73@gmail.com>
2026-01-05 17:11:00 +0530

    Retrying#2

--- modules/new-reserv/main.tf
+++ modules/new-reserv/main.tf
@@ -1,9 +1,9 @@
 resource "google_bigquery_reservation" "this" {
   name           = var.name
   project        = var.project_id
-  location       = var.location
   slot_capacity  = var.slot_capacity
   edition        = var.edition
+  location       = var.location
 
   lifecycle {
     prevent_destroy = true
